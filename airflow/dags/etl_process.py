503eb1747d6d8437d08240a3b163e3395222ce95
Antog <agambarte@onebittech.com>
2025-09-27 19:03:11 -0300

    modificación etl_process

--- airflow/dags/etl_process.py
+++ airflow/dags/etl_process.py
@@ -3,19 +3,16 @@ import datetime
 from airflow.decorators import dag, task
 
 markdown_text = """
-### ETL Process for Heart Disease Data
+### ETL Process for Stroke Prediction Dataset
 
-This DAG extracts information from the original CSV file stored in the UCI Machine Learning Repository of the 
-[Heart Disease repository](https://archive.ics.uci.edu/dataset/45/heart+disease). 
-It preprocesses the data by creating dummy variables and scaling numerical features.
-    
-After preprocessing, the data is saved back into a S3 bucket as two separate CSV files: one for training and one for 
-testing. The split between the training and testing datasets is 70/30 and they are stratified.
+This DAG extracts information from the Stroke Prediction dataset from Kaggle.
+It preprocesses the data by creating dummy variables for categorical columns and scaling numerical features.
+After preprocessing, the data is saved back into an S3 bucket (MinIO) as separate CSV files for training and testing.
 """
 
 
 default_args = {
-    'owner': "Facundo Adrian Lucianna",
+    'owner': "Group MLOps",
     'depends_on_past': False,
     'schedule_interval': None,
     'retries': 1,
@@ -25,19 +22,18 @@ default_args = {
 
 
 @dag(
-    dag_id="process_etl_heart_data",
-    description="ETL process for heart data, separating the dataset into training and testing sets.",
+    dag_id="process_etl_stroke_data",
+    description="ETL process for stroke data, separating the dataset into training and testing sets.",
     doc_md=markdown_text,
-    tags=["ETL", "Heart Disease"],
+    tags=["ETL", "Stroke Prediction"],
     default_args=default_args,
     catchup=False,
 )
-def process_etl_heart_data():
+def process_etl_stroke_data():
 
     @task.virtualenv(
         task_id="obtain_original_data",
-        requirements=["ucimlrepo==0.0.3",
-                      "awswrangler==3.6.0"],
+        requirements=["awswrangler==3.6.0"],
         system_site_packages=True
     )
     def get_data():
@@ -45,20 +41,16 @@ def process_etl_heart_data():
         Load the raw data from UCI repository
         """
         import awswrangler as wr
-        from ucimlrepo import fetch_ucirepo
         from airflow.models import Variable
 
-        # fetch dataset
-        heart_disease = fetch_ucirepo(id=45)
+        # Endpoint MinIO
+        # Endpoint MinIO
+        wr.config.s3_endpoint_url = "http://minio:9000"
 
-        data_path = "s3://data/raw/heart.csv"
-        dataframe = heart_disease.data.original
+        data_path = "s3://data/raw/healthcare-dataset-stroke-data.csv"
+        dataframe = wr.s3.read_csv(data_path)
 
-        target_col = Variable.get("target_col_heart")
-
-        # Replace level of heart decease to just distinguish presence 
-        # (values 1,2,3,4) from absence (value 0).
-        dataframe.loc[dataframe[target_col] > 0, target_col] = 1
+        target_col = Variable.get("target_col_stroke", default_var="stroke")
 
         wr.s3.to_csv(df=dataframe,
                      path=data_path,
@@ -86,23 +78,17 @@ def process_etl_heart_data():
 
         from airflow.models import Variable
 
-        data_original_path = "s3://data/raw/heart.csv"
-        data_end_path = "s3://data/raw/heart_dummies.csv"
+        data_original_path = "s3://data/raw/healthcare-dataset-stroke-data.csv"
+        data_end_path = "s3://data/raw/stroke_dummies.csv"
         dataset = wr.s3.read_csv(data_original_path)
 
         # Clean duplicates
         dataset.drop_duplicates(inplace=True, ignore_index=True)
-        # Drop NaN
-        dataset.dropna(inplace=True, ignore_index=True)
+        
+        # TODO: realizar imputación de nulos
 
-        # Force type in categorical columns
-        dataset["cp"] = dataset["cp"].astype(int)
-        dataset["restecg"] = dataset["restecg"].astype(int)
-        dataset["slope"] = dataset["slope"].astype(int)
-        dataset["ca"] = dataset["ca"].astype(int)
-        dataset["thal"] = dataset["thal"].astype(int)
-
-        categories_list = ["cp", "restecg", "slope", "ca", "thal"]
+        categories_list = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
+        dataset[categories_list] = dataset[categories_list].astype(str)
         dataset_with_dummies = pd.get_dummies(data=dataset,
                                               columns=categories_list,
                                               drop_first=True)
@@ -125,7 +111,7 @@ def process_etl_heart_data():
                 # Something else has gone wrong.
                 raise e
 
-        target_col = Variable.get("target_col_heart")
+        target_col = Variable.get("target_col_stroke",default_var="stroke")
         dataset_log = dataset.drop(columns=target_col)
         dataset_with_dummies_log = dataset_with_dummies.drop(columns=target_col)
 
@@ -155,21 +141,21 @@ def process_etl_heart_data():
         )
 
         mlflow.set_tracking_uri('http://mlflow:5000')
-        experiment = mlflow.set_experiment("Heart Disease")
+        experiment = mlflow.set_experiment("Stroke Prediction")
 
         mlflow.start_run(run_name='ETL_run_' + datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"'),
                          experiment_id=experiment.experiment_id,
-                         tags={"experiment": "etl", "dataset": "Heart disease"},
+                         tags={"experiment": "etl", "dataset": "Stroke Prediction"},
                          log_system_metrics=True)
 
         mlflow_dataset = mlflow.data.from_pandas(dataset,
-                                                 source="https://archive.ics.uci.edu/dataset/45/heart+disease",
+                                                 source="https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset",
                                                  targets=target_col,
-                                                 name="heart_data_complete")
+                                                 name="stroke_data_complete")
         mlflow_dataset_dummies = mlflow.data.from_pandas(dataset_with_dummies,
-                                                         source="https://archive.ics.uci.edu/dataset/45/heart+disease",
+                                                         source="https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset",
                                                          targets=target_col,
-                                                         name="heart_data_complete_with_dummies")
+                                                         name="stroke_data_complete_with_dummies")
         mlflow.log_input(mlflow_dataset, context="Dataset")
         mlflow.log_input(mlflow_dataset_dummies, context="Dataset")
 
@@ -192,11 +178,11 @@ def process_etl_heart_data():
                          path=path,
                          index=False)
 
-        data_original_path = "s3://data/raw/heart_dummies.csv"
+        data_original_path = "s3://data/raw/stroke_dummies.csv"
         dataset = wr.s3.read_csv(data_original_path)
 
-        test_size = Variable.get("test_size_heart")
-        target_col = Variable.get("target_col_heart")
+        test_size = float(Variable.get("test_size_stroke", default_var=0.2))
+        target_col = Variable.get("target_col_stroke", default_var="stroke")
 
         X = dataset.drop(columns=target_col)
         y = dataset[[target_col]]
@@ -206,10 +192,10 @@ def process_etl_heart_data():
         # Clean duplicates
         dataset.drop_duplicates(inplace=True, ignore_index=True)
 
-        save_to_csv(X_train, "s3://data/final/train/heart_X_train.csv")
-        save_to_csv(X_test, "s3://data/final/test/heart_X_test.csv")
-        save_to_csv(y_train, "s3://data/final/train/heart_y_train.csv")
-        save_to_csv(y_test, "s3://data/final/test/heart_y_test.csv")
+        save_to_csv(X_train, "s3://data/final/train/stroke_X_train.csv")
+        save_to_csv(X_test, "s3://data/final/test/stroke_X_test.csv")
+        save_to_csv(y_train, "s3://data/final/train/stroke_y_train.csv")
+        save_to_csv(y_test, "s3://data/final/test/stroke_y_test.csv")
 
     @task.virtualenv(
         task_id="normalize_numerical_features",
@@ -237,8 +223,8 @@ def process_etl_heart_data():
                          path=path,
                          index=False)
 
-        X_train = wr.s3.read_csv("s3://data/final/train/heart_X_train.csv")
-        X_test = wr.s3.read_csv("s3://data/final/test/heart_X_test.csv")
+        X_train = wr.s3.read_csv("s3://data/final/train/stroke_X_train.csv")
+        X_test  = wr.s3.read_csv("s3://data/final/test/stroke_X_test.csv")
 
         sc_X = StandardScaler(with_mean=True, with_std=True)
         X_train_arr = sc_X.fit_transform(X_train)
@@ -247,8 +233,8 @@ def process_etl_heart_data():
         X_train = pd.DataFrame(X_train_arr, columns=X_train.columns)
         X_test = pd.DataFrame(X_test_arr, columns=X_test.columns)
 
-        save_to_csv(X_train, "s3://data/final/train/heart_X_train.csv")
-        save_to_csv(X_test, "s3://data/final/test/heart_X_test.csv")
+        save_to_csv(X_train, "s3://data/final/train/stroke_X_train.csv")
+        save_to_csv(X_test, "s3://data/final/test/stroke_X_test.csv")
 
         # Save information of the dataset
         client = boto3.client('s3')
@@ -274,7 +260,7 @@ def process_etl_heart_data():
         )
 
         mlflow.set_tracking_uri('http://mlflow:5000')
-        experiment = mlflow.set_experiment("Heart Disease")
+        experiment = mlflow.set_experiment("Stroke Prediction")
 
         # Obtain the last experiment run_id to log the new information
         list_run = mlflow.search_runs([experiment.experiment_id], output_format="list")
@@ -291,4 +277,4 @@ def process_etl_heart_data():
     get_data() >> make_dummies_variables() >> split_dataset() >> normalize_data()
 
 
-dag = process_etl_heart_data()
+dag = process_etl_stroke_data()
