1659614d239b0978a0f93a2e08baf10c69f25bed
Jose Luis Diaz <diazjoseluis@gmail.com>
2025-10-18 13:15:33 -0300

    chore: s/lib/src/

--- dockerfiles/fastapi/app.py
+++ dockerfiles/fastapi/app.py
@@ -13,7 +13,7 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
-from lib import predict as model_predict
+from src import predict as model_predict
 
 app = FastAPI()
 
